0c5c87bc683951fc14a70834e214b0ebd918ce6b
YifanXu <yifanxu00@outlook.com>
2017-10-28 09:41:10 -0700

    death!!!

---
+++ MovementTesting/Assets/SpikeBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpikeBehavior : MonoBehaviour {
+
+    public string DeathSceneName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(object.ReferenceEquals(other.gameObject,PlayerInput.Player))
+        {
+            SceneManager.LoadScene(DeathSceneName);
+        }
+    }
+}

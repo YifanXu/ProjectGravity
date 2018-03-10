5703574c8c2ed75be79888e0ab1e092af62786c3
yifan <yifan@LAPTOP-D149ADF3>
2018-03-10 13:51:31 -0800

    Level 21-22

---
+++ MovementTesting/Assets/Scripts/TouchKillScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchKillScript : MonoBehaviour {
+
+    bool playerOnly = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(playerOnly && collision.gameObject == PlayerInput.Player)
+        {
+            PlayerInput.Die("Hit by a orange pedestrian.");
+        }
+        else if(!playerOnly && collision.gameObject.GetComponent<KillableEntityBehavior>() != null)
+        {
+            collision.gameObject.GetComponent<KillableEntityBehavior>().Damage(1);
+        }
+    }
+}

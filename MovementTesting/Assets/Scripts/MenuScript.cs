6323f34fe2d66ad044c4c2050ef3f910aa526999
yifan <yifan@LAPTOP-D149ADF3>
2018-03-11 16:34:28 -0700

    level connection fixs, title screen, yikeesssss

---
+++ MovementTesting/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+
+    public GameObject settings;
+    public GameObject credits;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LvlSelect()
+    {
+        MusicControll.instance.LoadLevel("LevelSelect");
+    }
+
+    public void Settings()
+    {
+        settings.SetActive(!settings.activeInHierarchy);
+    }
+
+    public void Credits()
+    {
+        credits.SetActive(!credits.activeInHierarchy);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
